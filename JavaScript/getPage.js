27c903e3efcabdd6e14d9fa2d17a44dc76ebd529
jbehr <jbehr04@yahoo.com>
2021-04-27 22:33:27 -0400

    working on length check

--- JavaScript/getPage.js
+++ JavaScript/getPage.js
@@ -7,19 +7,26 @@ baseTag.href = "//en.wikipedia.org";
 baseTag.target = "_blank";
 document.head.appendChild(baseTag);
 
+let styles = document.createElement('link');
+styles.rel = 'stylesheet';
+styles.href = '/w/load.php?lang=en&modules=ext.cite.styles%7Cext.discussionTools.init.styles%7Cext.uls.interlanguage%7Cext.visualEditor.desktopArticleTarget.noscript%7Cext.wikimediaBadges%7Coojs-ui.styles.icons-moderation%7Cskins.vector.styles.legacy%7Cwikibase.client.init&only=styles&skin=vector';
+document.head.appendChild(styles);
+
 let randPage = 'Main Page';
 let apiEndpoint = "https://en.wikipedia.org/w/api.php";
 let params = "action=query&list=random&rnlimit=1&rnnamespace=0&format=json";
 
 let rand = function(response) {
     randPage = response.query.random[0].title;
+    let url = `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles=${randPage}&exchars=1550`;
+    loadScript(url + '&callback=isShort');
 }
 
 let parser = function(response) {
-    console.log(response);
     let htmlString = response.parse.text['*'];
     let headhtml = response.parse.headhtml['*'];
     $("head").append($($.parseHTML(headhtml.trim())));
+    document.querySelector('link[href="/static/favicon/wikipedia.ico"]').remove();
     $("body").append($($.parseHTML(htmlString.trim())));
 }
 
@@ -33,7 +40,17 @@ function loadScript(url) {
     })
 }
 
+function isShort(response) {
+    for(let key in response.query.pages){
+        if(response.query.pages[key].extract.contains('NewPP limit report')){}//TODO: break async function and re-call rand
+    }
+}
+
 loadScript(apiEndpoint + "?" + params + "&callback=rand")
+    .then(function(script) { //load title
+        $("body").append(`<h1 id="firstHeading" class="firstHeading">${randPage}</h1>`);
+        $("body").append('<div id="siteSub" class="noprint">From Wikipedia, the free encyclopedia</div>');
+    })
     .then(function(script) {//wait for first script to load
         let url = "https://en.wikipedia.org/w/api.php?" +
             new URLSearchParams({
