4a9df0bd054aa25853c15a5188fffef0d99066fd
jbehr <jbehr04@yahoo.com>
2021-05-02 21:24:17 -0400

    finished isShort and working on prune

--- JavaScript/getPage.js
+++ JavaScript/getPage.js
@@ -1,3 +1,16 @@
+/**TODO:
+ *      >Fix contents table
+ *      >Remove tone tag
+ *      >Remove edit tags
+ *      >Remove bottom links:
+ *          -See also
+ *          -External Links
+ *          -References
+ *          -Other Sources
+ *          -Appendices
+ *      >Link changing
+**/
+
 var jQueryScript = document.createElement('script');
 jQueryScript.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js";
 document.head.appendChild(jQueryScript);
@@ -14,43 +27,69 @@ document.head.appendChild(styles);
 
 let randPage = 'Main Page';
 let apiEndpoint = "https://en.wikipedia.org/w/api.php";
-let params = "action=query&list=random&rnlimit=1&rnnamespace=0&format=json";
+let params = "action=query&list=random&rnlimit=10&rnnamespace=0&format=json";
+let randResp = ['penis muncher', 'beans', 'lmao'];
+let randIndex = 0;
 
 let rand = function(response) {
     randPage = response.query.random[0].title;
-    let url = `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles=${randPage}&exchars=1550`;
-    loadScript(url + '&callback=isShort');
+    randResp = response.query.random;
+    randIndex++;
 }
 
 let parser = function(response) {
-    let htmlString = response.parse.text['*'];
+    let htmlString = $($.parseHTML(response.parse.text['*']));
     let headhtml = response.parse.headhtml['*'];
-    $("head").append($($.parseHTML(headhtml.trim())));
-    document.querySelector('link[href="/static/favicon/wikipedia.ico"]').remove();
-    $("body").append($($.parseHTML(htmlString.trim())));
+    htmlString = prune(htmlString);
+    if(isShort(htmlString)) {
+        randPage = randResp[randIndex].title;
+        console.log(randPage);
+        randIndex++;
+        let url = "https://en.wikipedia.org/w/api.php?" +
+            new URLSearchParams({
+                action: "parse",
+                page: randPage,
+                format: "json",
+                prop: "text|headhtml",
+            });
+        loadScript(url + "&callback=parser");
+    } else {
+        $("body").append(`<h1 id="firstHeading" class="firstHeading">${randPage}</h1>`);
+        $("body").append('<div id="siteSub" class="noprint">From Wikipedia, the free encyclopedia</div>');
+        $("head").append($($.parseHTML(headhtml.trim())));
+        document.querySelector('link[href="/static/favicon/wikipedia.ico"]').remove();
+        $("body").append($($.parseHTML(htmlString.trim())));
+    }
+}
+
+function isShort(html) { //TODO: Make this work with html object
+    let count = 0;
+    let str = html;
+    while(str.indexOf('<a href="/wiki/') !== str.lastIndexOf('<a href="/wiki/')) {
+        count++;
+        let sub = str.substring((str.indexOf('<a href="/wiki/')));
+        str = sub.substring(sub.indexOf('</a>') + 4);
+    }
+    return count < 12;
+}
+
+function prune(html) { //TODO: see top list
+    html.filter(':contains("ambox")').remove();
+    html.filter(':contains("asbox")').remove();
+    return html;
 }
 
 function loadScript(url) {
     return new Promise(function(resolve, reject) {
         let script = document.createElement("script"); // Dynamically create a "script" tag
         script.src = url; // Point to the query string
+        document.body.appendChild(script); // Add the script tag to the document
         script.onload = () => resolve(script);
         script.onerror = () => reject(new Error(`Script load error for ${url}`));
-        document.body.appendChild(script); // Add the script tag to the document
     })
-}
-
-function isShort(response) {
-    for(let key in response.query.pages){
-        if(response.query.pages[key].extract.contains('NewPP limit report')){}//TODO: break async function and re-call rand
-    }
 }
 
 loadScript(apiEndpoint + "?" + params + "&callback=rand")
-    .then(function(script) { //load title
-        $("body").append(`<h1 id="firstHeading" class="firstHeading">${randPage}</h1>`);
-        $("body").append('<div id="siteSub" class="noprint">From Wikipedia, the free encyclopedia</div>');
-    })
     .then(function(script) {//wait for first script to load
         let url = "https://en.wikipedia.org/w/api.php?" +
             new URLSearchParams({
@@ -59,7 +98,7 @@ loadScript(apiEndpoint + "?" + params + "&callback=rand")
                 format: "json",
                 prop: "text|headhtml",
             });
-        return loadScript(url + '&callback=parser')
-    });
+        return loadScript(url + '&callback=parser');
+    })
 
 
